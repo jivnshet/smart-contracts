d520047e1c80cb672a585fcbb8508c6bafbedec8
Jivn Shet <107192090+jivnshet@users.noreply.github.com>
2022-10-28 15:29:24 +0530

    Update NFTStaking.sol

--- NFTStaking.sol
+++ NFTStaking.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import "https://github.com/jivnshet/smart-contracts/blob/main/NTHRRewards.sol";
 import "https://github.com/jivnshet/smart-contracts/blob/main/Collection.sol";
